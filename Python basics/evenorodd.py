ed664017e25b385997739110c9d6aaf1ace99c5f
Jashwanth G <codesjash@gmail.com>
2025-06-11 11:10:38 +0530

    initial commit

---
+++ Python basics/evenorodd.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number :")) # taking input fro user
+
+if num % 2 == 0:   # check whether the number is divided by 2
+    print(f"{num} is a even number")   # if yes it is even
+else:
+    print(f"{num} is a odd number")    # if not it is a odd
